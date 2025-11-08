34aa825632ffea9fd46d89ff2bd050dc46896357
find1134 <3989613839@qq.com>
2025-11-08 09:08:12 +0800

    删除所有文件

--- README.md
+++
@@ -1,2 +0,0 @@
-# plants-VS-zombies_v.Python
-一个使用Python开发的植物大战僵尸版本，目前正在更新中 A version of Plants vs. Zombies developed using Python, currently being updated.
--- src/game/levels/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-关卡管理系统
-"""
-
-from .level_manager import LevelManager
-from .level_data import LevelData, get_level_data
-
-__all__ = ['LevelManager', 'LevelData', 'get_level_data']
